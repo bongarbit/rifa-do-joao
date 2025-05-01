2715c3df3ce507e665a345092e36b0c850fbcaf1
Bongarbit - Labortatório de Tecnologias Digitais da Xambá <bongarbit@gmail.com>
2025-04-30 22:40:11 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const soldNumbers = [3, 13, 14, 15, 16, 22, 23, 24, 25, 26, 33, 34, 35, 36, 37, 64, 65, 66, 92, 93, 98];
+const soldNumbers = [5, 11, 12, 15, 17];
 
 function renderGrid() {
   const grid = document.getElementById("grid");
